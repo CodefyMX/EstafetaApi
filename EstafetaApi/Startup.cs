fe59cd92468ce208bf756405729f5ac9a603031a
Alan Torres <temp_peri@hotmail.com>
2017-01-06 09:07:29 -0600

    Project cleaned

--- EstafetaApi/Startup.cs
+++ EstafetaApi/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(EstafetaApi.Startup))]
+[assembly: OwinStartup(typeof(EstafetaApi.Startup))]
 namespace EstafetaApi
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+
         }
     }
 }
